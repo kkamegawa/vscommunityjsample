19dd1193ddd818940a4c95c54a0ff4f551707f99
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-11-19 19:04:41 +0900

    add Windows eventlog sample

---
+++ demoproject/01_demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _01_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLog = new EventLog();
+            myLog.Log = "Application";
+            var collection = myLog.Entries;
+            Console.WriteLine(collection[0].Message);
+        }
+    }
+}
