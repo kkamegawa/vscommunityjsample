718cb1852e6586bc7d75a4d81eeea6db61f528be
kkamegawa <kkamegawa@clueup.org>
2020-11-19 21:50:16 +0000

    update logging

--- demoproject/02_demo/Program.cs
+++ demoproject/02_demo/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 
 namespace _02_demo
 {
-    class Program
+    public class MyEventSource : EventSource
+    {
+        public static MyEventSource Log = new MyEventSource();
+        public void Startup() { WriteEvent(1); }
+        public void OpenFileStart(string fileName) { WriteEvent(2, fileName); }
+        public void OpenFileStop() { WriteEvent(3);  }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string logFileName = new MyEventSource.GetName(typeof(MyEventSource));
+            IEnumerable<EventSource> eventSources = MyEventSource.GetSources();
+            MyEventSource.Log.Startup();
+            MyEventSource.Log.OpenFileStart("SampleLog");
+            MyEventSource.Log.OpenFileStop();
         }
     }
 }
