0ded1f3de0dac42e437c281658510fdc5388f5ff
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-11-23 21:57:50 +0900

    add memory leak project

---
+++ demoproject/03_memoryleak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _03_memoryleak
+{
+    public class MemoryManagement
+    {
+        private IntPtr _buffer;
+
+        public MemoryManagement()
+        {
+            _buffer = Marshal.AllocHGlobal(1000);
+        }
+        public void AddMemory(int value)
+        {
+            _buffer = new IntPtr(value);
+        }
+    }
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var memory = new MemoryManagement();
+            memory.AddMemory(100);
+
+        }
+    }
+}
