e9999a7eec8e99bebed05b2f0222067e5b647f5a
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-11-27 23:09:09 +0900

    add aspnetcore project

---
+++ demoproject/04_mvcapp/MessageBoardSample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly MessageBoardModel db = new MessageBoardModel();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new IndexModel
+            {
+                Posted = db.Messages
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(IndexModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var newMessage = model.Input;
+                newMessage.Posted = DateTime.UtcNow;
+                db.Messages.Add(newMessage);
+                await db.SaveChangesAsync();
+
+                ModelState.Clear();
+                ViewBag.Posted = true;
+            }
+
+            return View(new IndexModel
+            {
+                Posted = db.Messages
+            });
+        }
+    }
+}
---
+++ demoproject/04_mvcapp/MessageBoardSample/Models/MessageBoardModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MessageBoardSample.Models
+{
+    public class MessageBoardModel : DbContext
+    {
+        public MessageBoardModel()
+            : base("name=MessageBoardModel")
+        {
+        }
+
+        public virtual DbSet<MessageModel> Messages { get; set; }
+    }
+}
