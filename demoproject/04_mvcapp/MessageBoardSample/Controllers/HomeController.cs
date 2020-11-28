51a8c551f4bec44e302c441b0b3bf37924c0a270
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-11-28 19:00:07 +0900

    migrate .net 5

--- demoproject/04_mvcapp/MessageBoardSample/Controllers/HomeController.cs
+++ demoproject/04_mvcapp/MessageBoardSample/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using MessageBoardSample.Models;
 
 namespace MessageBoardSample.Controllers
 {
---
+++ demoproject/04_mvcapp/MessageBoardSample/Models/IndexModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MessageBoardSample.Models
+{
+    public class IndexModel
+    {
+        public MessageModel Input { get; set; }
+        public IEnumerable<MessageModel> Posted { get; set; }
+    }
+
+    public class MessageModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Key { get; set; }
+
+        [DisplayName("ハンドル名")]
+        [Required(ErrorMessage = "ハンドル名を入力してください。")]
+        [MaxLength(20, ErrorMessage = "ハンドル名が長すぎます。")]
+        public string Username { get; set; }
+
+        [DisplayName("メッセージ")]
+        [Required(ErrorMessage = "メッセージを入力してください。")]
+        [MaxLength(140, ErrorMessage = "メッセージが長すぎます。")]
+        public string Message { get; set; }
+
+        public DateTime Posted { get; set; }
+    }
+}
