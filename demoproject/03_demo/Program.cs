d88eaf0485143c175a69bc76b492f2eb5ec37be3
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-12-02 06:09:20 +0900

    add ef

---
+++ demoproject/03_demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ demoproject/03_demonetfx48/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_demonetfx48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AdventureWorks2016Entities())
+            {
+                var query = from p in db.SalesOrderDetail
+                    from q in db.SalesOrderHeader
+
+                    orderby p.ProductID
+                    select p;
+                var lists = query.ToListAsync();
+                Console.WriteLine(lists);
+            }
+        }
+    }
+}
