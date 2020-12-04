fa4cec9eb2124d5b86e2d67e0bdfac56f2e9fb9e
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-12-05 02:33:51 +0900

    feature add paging sort search

--- demoproject/WebApptest/Controllers/HomeController.cs
+++ demoproject/WebApptest/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApptest.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApptest.Data;
+using WebApptest.Models.SchoolViewModels;
 
 namespace WebApptest.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +35,18 @@ namespace WebApptest.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
