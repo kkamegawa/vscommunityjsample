b4fbd30fc6156caad1c39da9082ec95f2f97dc3c
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2020-12-05 01:02:26 +0900

    add docker and WSL2 project

---
+++ demoproject/00_envcheck/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine($"OSVersion:{Environment.OSVersion}");
+Console.WriteLine($"Processors:{Environment.ProcessorCount}");
+Console.WriteLine($"WorkingSet:{Environment.WorkingSet}");
+
